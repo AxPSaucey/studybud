468c6673ca8708f6e9e29366d90188a4b420801a
Andy Pham <apham3272@gmail.com>
2024-09-23 18:57:28 -0400

    User Registration

--- base/urls.py
+++ base/urls.py
@@ -5,6 +5,7 @@ urlpatterns = [
 
     path('login/', views.loginPage, name='login'),
     path('logout/', views.logoutUser, name='logout'),
+    path('register/', views.registerPage, name='register'),
 
     path('', views.home, name="home"),
     path('room/<str:pk>/', views.room, name="room"),
